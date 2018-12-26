4bf943d7feee78fde27918e78b8ef5cb9c3e5dc7
Pradyot Prakash <35835404+pradyotprksh@users.noreply.github.com>
2018-12-26 18:48:26 +0530

    Update ApiUtils.java

--- app/src/main/java/a3embed/example/com/loginapi/Helper/ApiUtils.java
+++ app/src/main/java/a3embed/example/com/loginapi/Helper/ApiUtils.java
@@ -4,7 +4,7 @@ public class ApiUtils {
 
     private ApiUtils() {}
 
-    public static final String BASE_URL = "https://api.flexyapp.com/driver/";               // URL
+    public static final String BASE_URL = "<<ENTER URL HERE>>";               // URL
 
     public static RetroEndPoints getAPIService() {
         return RetrofitInstance.getClient(BASE_URL).create(RetroEndPoints.class);
